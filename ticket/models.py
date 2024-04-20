d334bc54048ab9b3159e4ff54847313ca7989dbe
sinthushan <sinthushan@gmail.com>
2024-04-20 16:53:25 -0400

    basic model setup

---
+++ ticket/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.utils.translation import gettext_lazy
+
+from datetime import datetime
+
+from worker.models import Worker
+
+class Catergory(models.Model):
+    category_name = models.CharField(max_length=250)
+    duration = models.DurationField()
+
+class Ticket(models.Model):
+
+    class TicketStatus(models.TextChoices):
+        OPEN = "OP", gettext_lazy("Open")
+        ASIGNED = "AS", gettext_lazy("Asigned")
+        REJECTED = "RE", gettext_lazy("Rejected")
+        COMPLETED = "CO", gettext_lazy("Completed")
+
+    summary = models.TextField()
+    details = models.TextField()
+    status = models.CharField(max_length=2, choices=TicketStatus.choices, default=TicketStatus.OPEN)
+    creator = models.CharField(max_length=250) # want to be able to use this system with other systesm and therefore the creator can not be a worker
+    category = models.ForeignKey(Catergory, on_delete=models.SET_DEFAULT)
+    assigned_to =  models.ForeignKey(Worker, on_delete=models.SET_NULL, null=True, blank=True, related_name="assigned_tickets")
+    completed_by =  models.ForeignKey(Worker, on_delete=models.SET_NULL, null=True, blank=True, related_name="completed_tickets" )
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    def assign_ticket(self, worker: Worker):
+        if self.status == self.TicketStatus.ASIGNED or self.status == self.TicketStatus.OPEN:
+            self.status = self.TicketStatus.ASIGNED
+            self.assigned_to = worker
+            self.updated = datetime.now()
+            return f"{ticket.id} has been assigned to {worker.full_name}"
+
+    def close_ticket(self, worker: Worker):
+        self.status = self.TicketStatus.Completed
+        self.assigned_to = None
+        self.completed_by = worker
+        self.updated = datetime.now()
+    
+    def close_ticket(self):
+        self.status = self.TicketStatus.REJECTED
+        self.assigned_to = None
+        self.updated = datetime.now()
--- worker/models.py
+++ worker/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Team(models.Model):
+    team_name = models.CharField(max_length=250)
+
+    def rank_workers_by_availability(self) -> list[str]:
+        worker_availability = {}
+        for worker in self.workers:
+            worker_availability[worker.fullname] =  worker.get_availability()
+        return  list(dict(sorted(worker_availability.items(), key=lambda item: item[1], reverse=True)).keys()) #is there a more readable way of doing this????
+
+class Worker(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="worker")
+    logged_in = models.BooleanField()
+    team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True, blank=True, related_name="workers")
+    work_hours = models.IntegerField()
+
+    def get_availability(self):
+        hours_allocated = 0
+        for ticket in self.assigned_tickets:
+            hours_allocated  += ticket.category.duration
+        return self.work_hours - hours_allocated
+    
